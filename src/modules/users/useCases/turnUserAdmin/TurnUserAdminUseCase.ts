152e723869d495aafe69ebd761ff8ce993af6eb7
vkin <victorrealm@gmail.com>
2023-02-04 12:47:12 -0300

    feat: implements TurnUserAdminUseCase

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,19 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    try {
+      const user = this.usersRepository.findById(user_id);
+
+      if (user) {
+        return this.usersRepository.turnAdmin(user);
+      }
+
+      return null;
+    } catch (error) {
+      console.error(error);
+
+      return null;
+    }
   }
 }
 
