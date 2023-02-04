336a9e62200c92fa3edfc64a52a687ddb9894a4d
vkin <victorrealm@gmail.com>
2023-02-04 14:33:00 -0300

    fix: alter user routes creation

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -7,12 +7,20 @@ import { turnUserAdminController } from "../modules/users/useCases/turnUserAdmin
 
 const usersRoutes = Router();
 
-usersRoutes.post("/", createUserController.handle);
+usersRoutes.post("/", (request, response) =>
+  createUserController.handle(request, response)
+);
 
-usersRoutes.patch("/:user_id/admin", turnUserAdminController.handle);
+usersRoutes.patch("/:user_id/admin", (request, response) =>
+  turnUserAdminController.handle(request, response)
+);
 
-usersRoutes.get("/:user_id", showUserProfileController.handle);
+usersRoutes.get("/:user_id", (request, response) =>
+  showUserProfileController.handle(request, response)
+);
 
-usersRoutes.get("/", listAllUsersController.handle);
+usersRoutes.get("/", (request, response) =>
+  listAllUsersController.handle(request, response)
+);
 
 export { usersRoutes };
